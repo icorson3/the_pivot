f0726aaae7da252aad2eeb9d33e3dcb08fdf43d7
Parker P <exxcaelum@gmail.com>
2016-06-28 15:59:42 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,147 @@
-jcasimir = User.create(username: "jcasimir", password: "test", email: "jcasimir@example.com", name: "Jeff Casimir", address: "1510 Blake St", city: "Denver", state: "CO", zip: "80111")
-admin = User.create(username: "admin", password: "admin", role: 1, email: "jcasimir@example.com", name: "Jeff Casimir", address: "1510 Blake St", city: "Denver", state: "CO", zip: "80111")
+class Seed
 
-storage = Category.create(name: "storage")
-item_1 = storage.items.create(name: "Laser Disc", description: "Make great plates.", price: "5.99", image: "https://visualhunt.com/photos/m/5/laser-and-video-discs-24.jpg")
-item_2 = storage.items.create(name: "Floppy Disc", description: "Need to store a whopping 1.44 MB of data? This bad boy has got you covered.", price: "5.99", image: "https://visualhunt.com/photos/m/5/day-037-did-floppy-exist-once.jpg")
-item_3 = storage.items.create(name: "CD", description: "Like ninja stars but better.", price: "2.99", image: "https://visualhunt.com/photos/m/4/cd-dvd-computer-data-shiny-digital-disk.jpg")
+  def initialize
+    create_categories
+    create_items
+    create_users
+    create_vendors
+    create_orders
+    create_business_admins
+  end
+
+  def create_items
+    status = [0,1]
+    puts "Creating Items"
+    Categories.all.each do |category|
+      50.times do
+        Item.create!(
+          name: Faker::Commerce.product_name,
+          description: Faker::Hipster.sentence,
+          price: Faker::Number.decimal(2),
+          image: Faker::Avatar.image,
+          status: status.sample,
+          vendor_id: rand(1..20)
+          category_id: category.id
+        )
+      end
+    end
+    puts "Created Items"
+  end
+
+  def create_users
+    puts "Creating Users"
+    99.times do
+      username = Faker::Internet.user_name
+      User.create!(
+      username: username,
+      password: Faker::Internet.password,
+      role: 1
+      email: Faker::Internet.email('username'),
+      name: Faker::Name.name,
+      address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip_code,
+      vendor_id: nil
+      )
+    end
+    puts "Created Users"
+  end
+
+  def create_vendors
+    puts "Creating Vendors"
+    name = Faker::Company.name
+    20.times do Vendor.create!(
+      name: name,
+      description: Faker::Hipster.paragraph,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      status:[0,1,2,3],
+      slug: name.parameterize,
+      category_id: rand(1..10)
+      )
+    end
+    puts "Created Vendors"
+  end
+
+  def create_categories
+    puts "Creating Categories"
+    10.times do Category.create!(
+      name: Faker::Commerce.department(1)
+      kind: [0,1].sample
+      )
+    end
+    puts "Created Categories"
+  end
+
+  def create_orders
+    puts "Creating Orders"
+    User.all.each do |user|
+      10.times do
+        status = [0,1,2,3].sample
+        time = Faker::Date.between(2.years.ago, Date.today) if status == 3
+        item_ids = (1...500).to_a.sample(10)
+        Order.create!(
+        status: status,
+        finished_at: time,
+        user_id: user.id
+        )
+      end
+    end
+    puts "Created Orders"
+  end
+
+  def create_business_admins
+    Vendor.all.each do |vendor|
+      username = Faker::Internet.user_name
+      User.create!(
+      username: username,
+      password: Faker::Internet.password,
+      role: 2
+      email: Faker::Internet.email('username'),
+      name: Faker::Name.name,
+      address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip_code,
+      vendor_id: vendor.id
+      )
+    end
+  end
+end
+
+User.create!(username: "jmejia@turing.io",
+             password: "password",
+             role: 1
+             email: "jmejia@turing.io",
+             name: "Josh",
+             address: Faker::Address.street_address,
+             city: Faker::Address.city,
+             state: Faker::Address.state,
+             zip: Faker::Address.zip_code,
+             vendor_id: nil)
+User.create!(username: "nate@turing.io",
+             password: "password",
+             role: 2
+             email: "nate@turing.io",
+             name: "Nate",
+             address: Faker::Address.street_address,
+             city: Faker::Address.city,
+             state: Faker::Address.state,
+             zip: Faker::Address.zip_code,
+             vendor_id: 1)
+User.create!(username: "jorge@turing.io",
+             password: "password",
+             role: 3
+             email: "jorge@turing.io",
+             name: "Jorge",
+             address: Faker::Address.street_address,
+             city: Faker::Address.city,
+             state: Faker::Address.state,
+             zip: Faker::Address.zip_code,
+             vendor_id: nil)
+
+Seed.new
 
 
-electronics = Category.create(name: "electronics")
-item_4 = electronics.items.create(name: "Walkman", description: "Enjoy some tunes on your walkman.", price: "22.99", image: "https://visualhunt.com/photos/m/5/sony-sports-walkman-wm-f5-okinawa.jpg")
-item_5 = electronics.items.create(name: "VCR", description: "Now go find some VCR tapes!", price: "100.99", image: "https://visualhunt.com/photos/m/5/vcr.jpg")
-item_6 = electronics.items.create(name: "Discman", description: "Listen to all your favorite hits.", price: "10.99", image: "https://c4.staticflickr.com/3/2318/2051407819_794a95d465_b.jpg")
-
-game_systems = Category.create(name: "gaming")
-item_7 = game_systems.items.create(name: "Gameboy", description: "Remember discovering Tetris? Discover it again on this Gameboy.", price: "99.99", image: "https://c4.staticflickr.com/8/7481/16037341171_3db0f52a8d_k.jpg")
-item_8 = game_systems.items.create(name: "Atari", description: "Take it back to the 80's and play some Atari on this mint condition system.", price: "800.99", image: "https://c5.staticflickr.com/8/7678/17513196036_a6b2e4889f_k.jpg")
-item_9 = game_systems.items.create(name: "Nintendo", description: "Play some games on the original NES.", price: "1999.88", image: "https://visualhunt.com/photos/m/5/nintendo-nes.jpg")
-
-
-order_1 = jcasimir.orders.create(status: "paid")
-order_2 = jcasimir.orders.create(status: "paid")
-order_3 = jcasimir.orders.create(status: "paid")
-order_4 = jcasimir.orders.create(status: "ordered")
-
-cart_1 = Cart.new({item_1.id => 2, item_3.id => 1})
-cart_2 = Cart.new({item_2.id => 2, item_4.id => 1, item_5.id => 3})
-
-order_1.create_order_items(cart_1)
-order_2.create_order_items(cart_2)
-order_3.create_order_items(cart_1)
-order_4.create_order_items(cart_2)
-
-order_1.complete
-order_2.cancel
+# finished_at: status == 3 ? Faker::Date.between(2.years.ago, Date.today) : nil,
