e1cb6fbe33d65f3e4abd160f7fe387be121cb3e1
Ilana Corson <ilana.corson@gmail.com>
2016-06-28 18:00:47 -0600

    fixed the vendor routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,6 @@ Rails.application.routes.draw do
   get "/cart" => "cart_items#index", as: "cart"
   get "/dashboard" => "users#show", as: "dashboard"
   get "/favicon.ico" => "application#get_favicon"
-  get "/:vendor_slug", to: "vendors#show", as: "vendor"
   get "/:id" => "categories#show", as: "category"
+  get "/:vendor_slug", to: "vendors#show", as: "vendor"
 end
