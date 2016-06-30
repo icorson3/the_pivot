e7562780a5161a075506ac90298a5d2df60bd48f
Parker P <exxcaelum@gmail.com>
2016-06-30 10:09:50 -0600

    Updated the search bar Added clarity to the search terms, updated the styling, changed the path

--- spec/integration/user_can_search_for_items_with_search_bar_spec.rb
+++ spec/integration/user_can_search_for_items_with_search_bar_spec.rb
@@ -6,14 +6,14 @@ RSpec.feature "user searches for an item using search bar", type: :feature do
 
     visit root_path
 
-    fill_in "Search", with: vendors[0].items.first.name
+    fill_in "Search Items", with: vendors[0].items.first.name
     click_button "Search"
     expect(page).to have_no_content("Sorry, #{vendors[0].items.first.name} not found.")
     expect(page.text).to have_content("#{vendors[0].items.first.name}")
 
     expect(current_path).to eq("/search")
     click_on "#{vendors[0].items.first.name}"
-    expect(current_path).to eq("/items/#{vendors[0].items.first.id}")
+    expect(current_path).to eq("/#{vendors[0].slug}/items/#{vendors[0].items.first.id}")
     expect(page.text).to have_content("#{vendors[0].items.first.name}")
 
     expect(page.text).to have_content("#{vendors[0].items.first.name}")
