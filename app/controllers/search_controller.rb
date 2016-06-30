8f25fbd32add4408d80ccf77b08fa5fcb7e77432
Colin Osborn <colin.osborn@gmail.com>
2016-06-30 10:53:25 -0600

    updated search redirect

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     @items = Item.all
     if Item.search(params[:search]).order("name DESC") == []
       flash[:warning] = "Sorry, #{params[:search]} not found."
+      redirect_to items_path
     else
       @items = Item.search(params[:search]).order("name DESC")
     end
