911d35c4a97f3d995718c0c7a4f8901d4880ce5f
Ilana Corson <ilana.corson@gmail.com>
2016-06-30 22:08:11 -0600

    took the show out of items to fix test coverage for the item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,4 @@ class ItemsController < ApplicationController
     @per_page = params[:per_page] || Item.per_page
     @items = Item.paginate(:per_page => @per_page, :page => params[:page])
   end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
-
 end
