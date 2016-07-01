4e72070e5327655f20ce93397210154705cae9d7
Ilana Corson <ilana.corson@gmail.com>
2016-06-30 23:06:31 -0600

    vendor cant update info without all info present testing complete

--- spec/integration/vendor_updates_account_information_spec.rb
+++ spec/integration/vendor_updates_account_information_spec.rb
@@ -31,4 +31,30 @@ RSpec.feature "vendor updates information" do
 
     expect(current_path).to eq(vendor_show_path(user.vendor.slug))
   end
+
+  scenario "vendor tries to edit information but leaves a field blank" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
+    click_on "Apply to be a Vendor"
+
+    fill_in "Name", with: "Farmer Bob"
+    fill_in "Description", with: "fruits"
+    fill_in "City", with: "Denver"
+    fill_in "State", with: "CO"
+
+    click_on "Apply Now!"
+
+    click_on "Edit Vendor Information"
+
+    fill_in "Description", with: ""
+
+    click_on "Update"
+
+    expect(current_path).to eq(vendor_path(user.vendor))
+    expect(page).to have_content("Description can't be blank")
+  end
 end
