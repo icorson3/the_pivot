534009318f345649708c9ee9a6161540d2766022
Ilana Corson <ilana.corson@gmail.com>
2016-06-30 21:51:02 -0600

    fixed search testing coverage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,12 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @per_page = params[:per_page] || Item.per_page
+    @items = Item.paginate(:per_page => @per_page, :page => params[:page])
   end
 
   def show
     @item = Item.find(params[:id])
   end
+
+
 end
