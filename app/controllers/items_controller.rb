e5d1578a7411db8ae2ae3eeb933f4247c9ecff2a
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-02 20:34:40 -0600

    Move method to model and fix vendor item link

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @per_page = params[:per_page] || 15
-    approved_vendor_items = Item.joins(:vendor).where(vendors: {status: "approved"})
-    @items = approved_vendor_items.paginate(:per_page => @per_page, :page => params[:page])
+    @items = Item.from_approved_vendors.paginate(:per_page => @per_page, :page => params[:page])
   end
 end
