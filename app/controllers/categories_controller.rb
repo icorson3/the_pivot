3cfc96c0af42dc6242dc28f28d18b15bd063bbcf
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-04 15:48:13 -0600

    Fix category items pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @per_page = params[:per_page] || 15
     @category = Category.find_by(slug: params[:id])
-    @items = @category.items
+    @items = @category.items.from_approved_vendors.paginate(:per_page => @per_page, :page => params[:page])
   end
 end
