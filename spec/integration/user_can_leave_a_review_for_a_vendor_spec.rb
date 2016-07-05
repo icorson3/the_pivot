22617698192d6841eb085d0859698c53b4b6974a
Colin Osborn <colin.osborn@gmail.com>
2016-07-05 17:34:48 -0600

    vendor review funtional

--- spec/integration/user_can_leave_a_review_for_a_vendor_spec.rb
+++ spec/integration/user_can_leave_a_review_for_a_vendor_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.feature "user can leave a review for a vendor" do
   scenario "user can leave feedback for vendor" do
     user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
     vendor = create(:vendor)
 
     visit vendor_show_path(vendor.slug)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     fill_in "Author name", with: user.username
-    fill_in "review_body", with: "Great seller!"
+    fill_in "Body", with: "Great seller!"
 
     click_on "Submit"
 
@@ -19,5 +19,14 @@ RSpec.feature "user can leave a review for a vendor" do
       expect(page).to have_content("Great seller!")
       expect(page).to have_content(user.username)
     end
+  end
+  scenario "visitor cannot leave feedback for vendor" do
+    vendor = create(:vendor)
+
+    visit vendor_show_path(vendor.slug)
+
+    expect(page).to_not have_content("Author")
+    expect(page).to_not have_content("Body")
+    expect(page).to_not have_content("Submit")
   end
 end
