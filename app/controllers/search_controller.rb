8ad201479c818a8955eef73eedb0e51262beb374
Parker P <exxcaelum@gmail.com>
2016-07-06 16:44:16 -0600

    resolving merge conflict inside of the search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,13 +7,9 @@ class SearchController < ApplicationController
       @categories = Category.pluck(:name).uniq
       @vendors = Vendor.pluck(:name).uniq
     else
-<<<<<<< HEAD
-      @items = items.search(params[:search]).order("name DESC")
       @categories = Category.pluck(:name).uniq
       @vendors = Vendor.pluck(:name).uniq
-=======
       @items = Item.all.search(params[:search]).order("name DESC")
->>>>>>> a5a030c8d4ad0e584274c752706bccda7835c943
     end
   end
 end
