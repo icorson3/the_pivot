59cb8caea291d6be5df151388662f1e6040e3ce2
Ilana Corson <ilana.corson@gmail.com>
2016-07-06 14:52:36 -0600

    created table for vendor order items

--- spec/integration/vendor_sees_the_items_ordered_spec.rb
+++ spec/integration/vendor_sees_the_items_ordered_spec.rb
@@ -14,13 +14,17 @@ RSpec.feature "vendor sees items ordered" do
 
   scenario "vendor can see each their order index" do
     vendor = create(:vendor, status: "approved")
+    vendor_1 = create(:vendor, status: "approved")
+    item_1 = create(:item, vendor: vendor_1)
     item = create(:item, vendor: vendor)
     admin = create(:user, role: 1, vendor: vendor)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit vendor_item_path(vendor.slug, item)
+    click_on "Add to cart"
 
+    visit vendor_item_path(vendor_1.slug, item_1)
     click_on "Add to cart"
 
     visit cart_path
@@ -35,5 +39,6 @@ RSpec.feature "vendor sees items ordered" do
     expect(page).to have_content("#{vendor.name}'s Orders")
     expect(current_path).to eq(vendor_orders_path(vendor.slug))
     expect(page).to have_content(vendor.order_items.first.item.name)
+    expect(page).to_not have_content(vendor_1.order_items.first.item.name)
   end
 end
