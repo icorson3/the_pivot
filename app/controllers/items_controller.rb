d95f14275c090ba3727cac6907686b55f964a797
Robbie Jaeger <jaeger.rob@gmail.com>
2016-07-06 16:35:11 -0600

    Remove ruby pagination and gem

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @per_page = params[:per_page] || 15
-    @items = Item.from_approved_vendors.paginate(:per_page => @per_page, :page => params[:page])
+    @items = Item.from_approved_vendors
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,11 @@
 class SearchController < ApplicationController
 
   def index
-      @per_page = params[:per_page] || 15
-      # @items = items
     if Item.search(params[:search]).order("name DESC") == []
       flash[:warning] = "Sorry, #{params[:search]} not found."
       redirect_to items_path
     else
-      @items = items.search(params[:search]).order("name DESC")
+      @items = Item.all.search(params[:search]).order("name DESC")
     end
   end
-
-  private
-    def items
-      Item.paginate(:per_page => @per_page, :page => params[:page])
-    end
 end
