273a219a4cb2816da6090c85e20b0a6d790700b8
Parker P <exxcaelum@gmail.com>
2016-07-06 16:43:32 -0600

    resolving merge conflict inside of the items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,7 @@
 class ItemsController < ApplicationController
   def index
-<<<<<<< HEAD
-    @per_page = params[:per_page] || 15
-    @items = Item.from_approved_vendors.paginate(:per_page => @per_page, :page => params[:page])
     @categories = Category.pluck(:name).uniq
     @vendors = Vendor.pluck(:name).uniq
-=======
     @items = Item.from_approved_vendors
->>>>>>> a5a030c8d4ad0e584274c752706bccda7835c943
   end
 end
